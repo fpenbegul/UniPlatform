b191fe4ef8d8060bda65c12abf251d7032759aa1
Furkan Pen <->
2019-12-16 01:25:49 +0300

    sadece onaylı duyurular listeleniyor
    ana sayfada sadece onaylı duyurular listeleniyor

--- UniPlatform/Controllers/HomeController.cs
+++ UniPlatform/Controllers/HomeController.cs
@@ -1,15 +1,22 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UniPlatform.Models;
 
 namespace UniPlatform.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
+            List<Duyurular> duyurular = db.Duyurular.Where(x => x.OnayliMi == true).Take(10).ToList();
+            ViewBag.duyurular = duyurular;
+
             return View();
         }
 
